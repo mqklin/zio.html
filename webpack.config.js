57cb41f6177c5f5f03b7c73ac843c4da851b666e
gkio <webdevra@bk.ru>
2017-08-12 16:37:45 +0400

    webpack + initialize file for import

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const sass = require("./webpack/sass.config");
+
+
+const PATHS = {
+  source: path.join(__dirname),
+  build: path.join(__dirname, "build")
+};
+
+const common = merge([
+  {
+    entry: PATHS.source + "/app.js",
+    output: {
+      path: PATHS.build,
+      filename: "main.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2016"]
+            }
+          }
+        },
+        {
+          test: /\.png$/,
+          loaders: ["file-loader?name=i/[hash].[ext]"]
+        }
+      ]
+    },
+    resolve: {
+      modules: ["node_modules"]
+    }
+  }
+]);
+
+module.exports = merge([common, sass()]);
